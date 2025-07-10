af4fba07e9185516dd44f70f070b337d964a0082
INJAMAM <injamamul937@gmail.com>
2025-07-10 15:36:16 +0600

    update productController

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,7 +3,7 @@ import { sql } from "../config/db.js";
 export const getLaptops = async (req, res) => {
   try {
     const products = await sql.query(
-      "SELECT * FROM products WHERE category = 'laptop' ORDER BY id limit 20"
+      "SELECT id, name, price, main_image,description FROM products WHERE category = 'laptop' ORDER BY id limit 20"
     );
     res.json(products.rows);
   } catch (error) {
