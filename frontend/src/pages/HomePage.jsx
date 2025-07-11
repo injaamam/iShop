69357cb6123ead41d7de4861829d8de5bb73db83
INJAMAM <injamamul937@gmail.com>
2025-07-11 16:33:39 +0600

    homepage basic styling done

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,12 +3,26 @@ import { categories } from "../constant/categories";
 
 function HomePage() {
   return (
-    <div>
-      {categories.map((cat) => (
-        <Link key={cat} to={`/${cat}`}>
-          {cat}
-        </Link>
-      ))}
+    <div className="mx-5 md:mx-8 lg:mx-10">
+      <div className="flex flex-col gap-2 items-center mt-10">
+        <h2 className="text-xl font-semibold">Featured Category</h2>
+        <h3 className="text-md font-medium">
+          Get Your Desired Product from Featured Category!
+        </h3>
+      </div>
+      <div className="flex justify-center mt-10">
+        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-4">
+          {categories.map((cat) => (
+            <Link
+              key={cat}
+              to={`/${cat}`}
+              className="flex items-center justify-center h-16 border rounded text-center"
+            >
+              {cat}
+            </Link>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
