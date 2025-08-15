94aeeadb28f1334f0f65299c2d0706e371b6bf0a
INJAMAM <injamamul937@gmail.com>
2025-08-16 03:23:26 +0600

    minor tweak

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -10,8 +10,8 @@ function HomePage() {
   }, []);
 
   return (
-    <div className="mx-5 md:mx-8 lg:mx-10">
-      <div className="flex flex-col gap-2 items-center mt-10">
+    <div className="mx-5 md:mx-8 lg:mx-10 pt-3">
+      <div className="flex flex-col gap-2 items-center">
         <h2 className="text-xl font-semibold">Featured Category</h2>
         <h3 className="text-md font-medium">
           Get Your Desired Product from Featured Category!
