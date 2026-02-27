101c5c052588a3c9204fb33a0ccb83b005ace923
INJAMAM <injamamul937@gmail.com>
2026-02-27 23:30:40 +0600

    replace getCategories with hardcoded category list

--- frontend/src/constant/getCategories.js
+++ frontend/src/constant/getCategories.js
@@ -1,26 +1,29 @@
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-import axios from "axios";
+// const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+// import axios from "axios";
+
+// export async function getCategories() {
+//   const categories = await axios.get(`${BACKEND_URL}/categories`);
+//   return categories.data;
+// }
 
 export async function getCategories() {
-  const categories = await axios.get(`${BACKEND_URL}/categories`);
-  return categories.data;
+  const categories = [
+    "monitor",
+    "smart-watch",
+    "pen-drive",
+    "earbuds",
+    "gaming-chair",
+    "headphone",
+    "speaker-and-home-theater",
+    "keyboard",
+    "gaming-console",
+    "stylus",
+    "server",
+    "laptop",
+    "mobile-phone-charger-adapter",
+    "tablet-pc",
+    "mouse",
+    "camera",
+  ];
+  return categories;
 }
-
-// export const categories = [
-//   "monitor",
-//   "smart-watch",
-//   "pen-drive",
-//   "earbuds",
-//   "gaming-chair",
-//   "headphone",
-//   "speaker-and-home-theater",
-//   "keyboard",
-//   "gaming-console",
-//   "stylus",
-//   "server",
-//   "laptop",
-//   "mobile-phone-charger-adapter",
-//   "tablet-pc",
-//   "mouse",
-//   "camera",
-// ];
